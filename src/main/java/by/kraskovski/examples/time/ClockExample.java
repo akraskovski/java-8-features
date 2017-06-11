59fa1e714609f410836435b1fa5158fcbb5aa615
akraskovski <artem.kraskovski@gmail.com>
2017-06-11 13:26:04 +0300

    chromo units in the date

--- src/main/java/by/kraskovski/examples/time/ClockExample.java
+++ src/main/java/by/kraskovski/examples/time/ClockExample.java
@@ -4,12 +4,16 @@ import java.time.Clock;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 public class ClockExample {
 
     public static void main(String... args) {
 //        getClockAtUTC();
-
+//        getLocalDate();
+//        getLocalTime();
+//        getLocalDateTime();
+//        testChromoUnits();
     }
 
     private static Clock getClockAtUTC() {
@@ -41,5 +45,27 @@ public class ClockExample {
 
         System.out.println(localDateTime);
         System.out.println(localDateTimeFromClock);
+    }
+
+    private static void testChromoUnits() {
+        //Get the current date
+        LocalDate today = LocalDate.now();
+        System.out.println("Current date: " + today);
+
+        //add 1 week to the current date
+        LocalDate nextWeek = today.plus(1, ChronoUnit.WEEKS);
+        System.out.println("Next week: " + nextWeek);
+
+        //add 1 month to the current date
+        LocalDate nextMonth = today.plus(1, ChronoUnit.MONTHS);
+        System.out.println("Next month: " + nextMonth);
+
+        //add 1 year to the current date
+        LocalDate nextYear = today.plus(1, ChronoUnit.YEARS);
+        System.out.println("Next year: " + nextYear);
+
+        //add 10 years to the current date
+        LocalDate nextDecade = today.plus(1, ChronoUnit.DECADES);
+        System.out.println("Date after ten year: " + nextDecade);
     }
 }
