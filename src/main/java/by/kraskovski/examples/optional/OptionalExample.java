08c2f97c1937d6dcb53d617670b08bd98a44e501
akraskovski <artem.kraskovski@gmail.com>
2017-07-07 17:05:19 +0300

    optional methods example

--- src/main/java/by/kraskovski/examples/optional/OptionalExample.java
+++ src/main/java/by/kraskovski/examples/optional/OptionalExample.java
@@ -5,16 +5,47 @@ import java.util.Optional;
 public class OptionalExample {
 
     public static void main(String... args) {
-        Optional<String> fullName = Optional.ofNullable(null);
-        System.out.println( "Full Name is set? " + fullName.isPresent() );
-        System.out.println( "Full Name: " + fullName.orElse("[none]") );
-        System.out.println( fullName.map( s -> "Hey " + s + "!" ).orElse( "Hey Stranger!" ) );
+        optionalTypes();
+        isPresent();
+        ifPresent();
+        orElse();
+        orElseThrow();
 
-        System.out.println();
+    }
 
-        Optional< String > firstName = Optional.of( "Tom" );
-        System.out.println( "First Name is set? " + firstName.isPresent() );
-        System.out.println( "First Name: " + firstName.orElse("[none]") );
-        System.out.println( firstName.map( s -> "Hey " + s + "!" ).orElse( "Hey Stranger!" ) );
+    private static void optionalTypes() {
+        //Empty Optional container
+        Optional<String> empty = Optional.empty();
+        //Of some value, can't be null!
+        Optional<String> of = Optional.of("string");
+        //Of any value, can be null
+        Optional<String> ofNullable = Optional.ofNullable(null);
+    }
+
+    private static void isPresent() {
+        System.out.println(Optional.of("string").isPresent());
+        System.out.println(Optional.of(null).isPresent()); //Produce NullPointerException
+        System.out.println(Optional.ofNullable(null).isPresent());
+    }
+
+    private static void ifPresent() {
+        Optional<String> value = Optional.of("string");
+        value.ifPresent(s -> System.out.println(value.isPresent()));
+    }
+
+    private static void orElse() {
+        String nullValue = null;
+        String value = Optional.ofNullable(nullValue).orElse("string");
+        System.out.println(value);
+    }
+
+    private static void orElseThrow() {
+        String nullValue = null;
+        Optional.ofNullable(nullValue).orElseThrow(IllegalStateException::new);
+    }
+
+    private static void get() {
+        Optional<String> value = Optional.of("string");
+        String result = value.get();
     }
 }
